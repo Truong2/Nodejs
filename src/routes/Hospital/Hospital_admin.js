7f4548a0cff69d5c00bafe1b3edf5b13b28a0cf8
NV Tinh <tinhnvt1610@gmail.com>
2024-02-18 09:50:21 +0700

    get list hos remove token

--- src/routes/Hospital/Hospital_admin.js
+++ src/routes/Hospital/Hospital_admin.js
@@ -3,7 +3,7 @@ const formData = require("express-form-data");
 const hospital_service = require("../../controllers/Hospital/Hospital_admin")
 const func = require("../../services/function")
 
-router.get('/get_list_hospital', func.checkToken, hospital_service.getListHospital)
+router.get('/get_list_hospital', hospital_service.getListHospital)
 router.put('/update_hospital', func.checkToken, hospital_service.editHospital)
 router.put('/grant_role_hospital', func.checkToken,  hospital_service.grantRoleHospital)
 router.put('/change_status_hospital', func.checkToken,  hospital_service.changeStatus)
